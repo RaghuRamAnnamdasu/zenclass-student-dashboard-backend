bc1f19742d9e0b17d370ecc94325997110976023
RaghuRamAnnamdasu <annamdasuraghuram@gmail.com>
2022-08-30 06:39:55 +0530

    cors correction

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ app.use(function(req, res, next) {
     );
     res.setHeader("Access-Control-Allow-Origin","*");
     res.setHeader("Access-control-Allow-Methods", "GET, POST, PUT");
-    res.setHeader("Access-Control-Allow-Headers", "X-Requested-With, content-type");
+    res.setHeader("Access-Control-Allow-Headers", "Origin,X-Requested-With, content-type,Accept,Authorization");
     res.setHeader("Access-Control-Allow-Credentials", true);
     next();
 });
