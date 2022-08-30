a28bcea18c4d3f4b42a15788ec8163643641f366
RaghuRamAnnamdasu <annamdasuraghuram@gmail.com>
2022-08-30 06:26:09 +0530

    cors correction

--- index.js
+++ index.js
@@ -12,11 +12,19 @@ import { countRouter } from "./routes/count.js";
 const app = express();
 dotenv.config(); 
 // app.use(cors());
-app.use(
-        cors({
-          origin: "*",
-        })
-      );
+// app.use(
+//         cors({
+//           origin: "*",
+//         })
+//       );
+app.use((req, res, next) => {
+        res.setHeader("Access-Control-Allow-Origin", "*");
+        res.header(
+          "Access-Control-Allow-Headers",
+          "Origin, X-Requested-With, Content-Type, Accept"
+        );
+        next();
+      });
 app.use(express.json());
 
 const port = process.env.PORT || 4000;
